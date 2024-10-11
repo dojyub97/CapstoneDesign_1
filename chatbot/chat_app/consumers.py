f079b1d7add97a08a9322c63f84cad8be9ccd61f
도준엽 <dojyub97@gmail.com>
2024-10-11 18:17:33 +0900

    1. prompt engineering 폴더 위치 변경 2. 일반질문 통합 3. env파일생성(api key)

--- chatbot/chat_app/consumers.py
+++ chatbot/chat_app/consumers.py
@@ -1,21 +1,6 @@
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
-
-###########################gemini api를 쓰기 위한 위한 설정
-from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain_core.output_parsers import StrOutputParser
-from langchain_core.prompts import ChatPromptTemplate
-GEMINI_API_KEY = "AIzaSyCCSmH_iuExbKinCJYoHszuS0_QXJdanh8"
-llm = ChatGoogleGenerativeAI(
-    model="gemini-1.5-flash",
-    google_api_key=GEMINI_API_KEY)
-output_parser = StrOutputParser()
-prompt = ChatPromptTemplate.from_messages(
-    [ ("system","일반적인 답변을 해주세요"), 
-    ("user","{input}") ]
-)
-chain = prompt | llm | output_parser
-##############################gemini api를 쓰기 위한 위한 설정
+from prompt_engineering import general_text
 
 
 class ChatConsumer(AsyncWebsocketConsumer):
@@ -28,6 +13,6 @@ class ChatConsumer(AsyncWebsocketConsumer):
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json["message"]
-        response = await chain.ainvoke({"input" : message })
-
+        #response = await chain.ainvoke({"input" : message })
+        response = general_text.generate_response(message)
         await self.send(text_data=json.dumps({"message": response + "\n"}))
